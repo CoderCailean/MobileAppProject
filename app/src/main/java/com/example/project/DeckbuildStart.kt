8f9eff5147365a70ba4a7be0bdccc0fb2dd770b3
Dev-Josh-Graham <josh8.graham@gmail.com>
2023-11-20 16:28:47 -0600

    Functional API request

--- app/src/main/java/com/example/project/DeckbuildStart.kt
+++ app/src/main/java/com/example/project/DeckbuildStart.kt
@@ -1,6 +1,5 @@
 package com.example.project
 
-import android.content.Intent
 import androidx.activity.ComponentActivity
 import android.content.SharedPreferences
 import android.os.Bundle
@@ -13,27 +12,13 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Visibility
-import androidx.compose.material.icons.filled.VisibilityOff
-import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
@@ -45,28 +30,21 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.project.model.Card
-import com.example.project.room.CardEntity
-import com.example.project.room.ProjectDB
-import com.example.project.room.UserEntity
 import com.example.project.ui.theme.ProjectTheme
 import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.converter.scalars.ScalarsConverterFactory
+import CardItem
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 
 
 class DeckbuildStart : ComponentActivity(){
@@ -75,12 +53,12 @@ class DeckbuildStart : ComponentActivity(){
     lateinit var sharedPreferences: SharedPreferences
     var PREFS_KEY = "prefs"
 
-    private val BASE_URL = "https://api.fabdb.net/"
-    private val hash = "c5392d43c8f45e6e961ddf66dcfa36770a9964ad9e8feedd3e903d5d905821ae97e7de5578b09eb1bec008ef4731b457c525dc8d24b32ccfb086230c00e590b3"
+    private val BASE_URL = "https://raw.githubusercontent.com/the-fab-cube/flesh-and-blood-cards/main/json/english/"
+
 
     @Composable
-    fun cardList() : ArrayList<Card>{
-        var cards by remember { mutableStateOf(ArrayList<Card>()) }
+    fun getData() : ArrayList<CardItem>{
+        var cards by remember { mutableStateOf(ArrayList<CardItem>()) }
         val scope= rememberCoroutineScope()
 
         val retrofit = Retrofit.Builder()
@@ -88,26 +66,25 @@ class DeckbuildStart : ComponentActivity(){
             .baseUrl(BASE_URL)
             .build().create(FaBAPIService::class.java)
 
-        val time = System.currentTimeMillis().toString()
+        val cardData = retrofit.getCards()
 
-        val cardData = retrofit.getCards(time, hash)
 
         DisposableEffect(key1=Unit){
             scope.launch(Dispatchers.IO){
-                cardData.enqueue(object: Callback<ArrayList<Card>> {
+                cardData.enqueue(object: Callback<ArrayList<CardItem>> {
                     override fun onResponse(
-                        call: Call<ArrayList<Card>>,
-                        response: Response<ArrayList<Card>>
+                        call: Call<ArrayList<CardItem>>,
+                        response: Response<ArrayList<CardItem>>
                     ) {
                         if (response.isSuccessful) {
-                            cards = (response.body() ?: emptyList()) as ArrayList<Card>
-                            Log.i("Response", response.toString())
+                            cards = (response.body() ?: emptyList()) as ArrayList<CardItem>
+                            Log.i("asdfg", response.toString())
                         } else {
-                            //Handle a Error Response
+                            Log.i("asdfg", response.toString())
                         }
                     }
 
-                    override fun onFailure(call: Call<ArrayList<Card>>, t: Throwable) {
+                    override fun onFailure(call: Call<ArrayList<CardItem>>, t: Throwable) {
                         // Handle failure of the data object
                     }
                 })
@@ -133,7 +110,6 @@ class DeckbuildStart : ComponentActivity(){
                 ) {
                     val context = LocalContext.current
                     DeckbuildScreen()
-                    cardList()
                 }
             }
         }
@@ -144,6 +120,7 @@ class DeckbuildStart : ComponentActivity(){
     @Composable
     fun DeckbuildScreen()
     {
+        val cards = getData()
 
         Scaffold(
             topBar = {
@@ -174,42 +151,44 @@ class DeckbuildStart : ComponentActivity(){
                 verticalArrangement = Arrangement.Center
             )
             {
-                cardList()
-            }
-        }
-    }
+                Card(
+                    modifier = Modifier
+                        .padding(10.dp)
+                        .fillMaxWidth(maxOf(0.90f))
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.SpaceAround
+                    ) {
+                        Text("Sample Card", Modifier.padding(horizontal = 5.dp))
+                        Box(modifier = Modifier.fillMaxWidth(0.75f))
 
-//    @Composable
-//    fun CardList() {
-//
-//        LazyColumn(
-//            modifier = Modifier.fillMaxWidth(),
-//            verticalArrangement = Arrangement.Center,
-//            horizontalAlignment = Alignment.CenterHorizontally
-//        ) {
-//            //Add Card values here
-//            items(items = cards) { item ->
-//                CardCard(card = item)
-//            }
-//        }
-//    }
-
-    @Composable
-    fun CardCard(card: Card) {
-        Card(
-            modifier = Modifier
-                .padding(10.dp)
-                .fillMaxWidth(maxOf(0.90f))
-        ) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(text = card.name, Modifier.padding(horizontal = 5.dp))
-                Box(modifier = Modifier.fillMaxWidth(0.75f))
-                IconButton(onClick = { /*TODO*/ }) {
-                    Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "View")
+                    }
                 }
+                LazyColumn(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    //Add Card values here
+                    items(items = cards) { item ->
+                        Card(
+                            modifier = Modifier
+                                .padding(10.dp)
+                                .fillMaxWidth(maxOf(0.90f))
+                        ) {
+                            Row(
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.SpaceAround
+                            ) {
+                                Text(text = item.name, Modifier.padding(horizontal = 5.dp))
+                                Box(modifier = Modifier.fillMaxWidth(0.75f))
+
+                            }
+                        }
+                    }
+                }
+
             }
         }
     }
